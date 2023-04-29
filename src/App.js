2babadab5ecdc8547c42da1ec7ed978390ac71bd
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-04-29 11:54:23 +0300

    apply code splitting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Layout from './layout';
-import Products from './pages/Products';
-import Categories from './pages/Categories';
-import ProductDetails from './pages/ProductDetalis';
-import ProductsTemplate from './pages/ProductsTemplates';
 import ProductsLayout from './layout/ProductsLayout';
 import { ProductsProvider } from './store';
-import Login from './pages/Login';
 import ProtectedRoutes from './routes/ProtectedRoutes';
+import Loading from './components/common/Loading';
+
+const Home = lazy(() => import('./pages/Home'));
+const Products = lazy(() => import('./pages/Products'));
+const ProductDetails = lazy(() => import('./pages/ProductDetalis'));
+const ProductsTemplate = lazy(() => import('./pages/ProductsTemplates'));
+const Login = lazy(() => import('./pages/Login'));
+const Categories = lazy(() => import('./pages/Categories'));
 
 function App() {
   return (
     <>
       <ProductsProvider>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path='/products' element={<ProductsLayout />}>
-                <Route index element={<Products />} />
-                <Route path=':id' element={<ProductDetails />} />
-                <Route path='template' element={<ProductsTemplate />} />
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path='/products' element={<ProductsLayout />}>
+                  <Route index element={<Products />} />
+                  <Route path=':id' element={<ProductDetails />} />
+                  <Route path='template' element={<ProductsTemplate />} />
+                </Route>
+                <Route path='/categories' element={<Categories />} />
               </Route>
-              <Route path='/categories' element={<Categories />} />
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </Suspense>
       </ProductsProvider>
     </>
   );
